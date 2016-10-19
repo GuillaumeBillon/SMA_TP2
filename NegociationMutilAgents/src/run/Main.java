a71f5d463213514fabbe04aef6871b6ea145e80b
EPULAPP56\Epulapp <joris0994@gmail.com>
2016-10-19 09:17:01 +0200

    ok

--- NegociationMutilAgents/src/run/Main.java
+++ NegociationMutilAgents/src/run/Main.java
@@ -3,10 +3,6 @@ package run;
 public class Main {
 	
 	public static void main(String[] args){
-<<<<<<< HEAD
-
-=======
 		System.out.println("Hello World");
->>>>>>> 197d01ea865c082cff9cc25256667b8032bd37b3
 	}
 }
